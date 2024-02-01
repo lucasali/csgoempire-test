6ae87c03ab0174344bec54b044517ba3e5bc0ea9
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-01 19:18:18 -0300

    Add Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="appFooter">
+    <span>API Version: 1.0</span>
+  </footer>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.appFooter {
+  position: fixed;
+  bottom: 0;
+  background-color: #F6F7F7;
+  height: 40px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0 40px;
+}
+
+span {
+  font-size: 12px;
+  font-weight: 600;
+  color: #4B5C68;
+}
+</style>
