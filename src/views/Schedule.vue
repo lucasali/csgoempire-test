b9dd5a920abbbaeabc9576e148d610dc981d89b0
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-01 19:18:56 -0300

    Add Schedule view component

---
+++ src/views/Schedule.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>League Schedule</h2>
+
+  <ScheduleTable :matches="leagueService.getMatches()" />
+</template>
+
+<script setup>
+import { inject } from 'vue'
+import ScheduleTable from '../components/ScheduleTable.vue'
+
+const leagueService = inject('leagueService')
+</script>
+
+<style scoped>
+h2 {
+  font-size: 24px;
+  color: #182c62;
+  text-align: center;
+  margin: 0 0 20px 0;
+}
+</style>
