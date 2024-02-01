71f3dccd24fc96e6ac9675d70973dde0a3b44957
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-01 19:18:48 -0300

    Add Leaderboard view and component

---
+++ src/views/Leaderboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>League Standings</h2>
+
+  <LeaderboardTable :leaderboard="leagueService.getLeaderboard()" />
+</template>
+
+<script setup>
+import { inject } from 'vue'
+import LeaderboardTable from '../components/LeaderboardTable.vue'
+
+const leagueService = inject('leagueService')
+</script>
+
+<style scoped>
+h2 {
+  font-size: 24px;
+  color: #182c62;
+  text-align: center;
+  margin: 0 0 20px 0;
+}
+</style>
