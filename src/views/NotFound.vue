10189de7804bedc9c3bb294b3744f8645c6f48e5
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-01 19:18:52 -0300

    Add NotFound.vue component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="notFound">
+    <img src="/images/404.png" alt="404 Image">
+  </section>
+</template>
+
+<style scoped>
+.notFound {
+  display: grid;
+  place-items: center;
+}
+</style>
