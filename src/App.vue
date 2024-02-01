5585380c956f87d7985d987161af3996b6e5b0e0
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-01 19:17:46 -0300

    Refactor App.vue component and add LeagueService functionality

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <template>
-  <div class="welcomeMessage">
-    Hi there 👋, <br /><br />      
-    Welcome to your test task. <br /><br />
-    Before you begin make sure to read the README file from the repository to make sure that your environment is properly set up. <br /><br />
-    Also please make sure to read the challenge instructions carefully. We advice that you push your source code to the repository frequently to avoid any loss of work.<br />
-    Once you are ready to submit the work, just go back to the challenge instructions page and click the "Submit Challenge" button.<br /><br />      
-    Good Luck and Have Fun! 🤞<br /><br /><br /><br />
-    <em><strong>NOTE:</strong> This page is only a welcome message and you should overwrite this page with the actuall solution implementation.</em>
+  <div>
+    <Header />
+    <main>
+      <RouterView />
+    </main>
+    <Footer />
   </div>
 </template>
 
-<style>
-@import "./index.css";
+<script setup>
+import { onMounted, reactive, provide } from 'vue'
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+import LeagueService from './services/LeagueService';
 
-.welcomeMessage {
-  margin: 120px auto;
-  width: 50%;
-  text-align: left;
-  font-size: 24px;
+const leagueService = reactive(new LeagueService())
+
+provide('leagueService', leagueService)
+
+async function fetchToken() {
+  const response = await fetch('http://localhost:3001/api/v1/getAccessToken')
+  const data = await response.json()
+
+  setToken(data.access_token)
+}
+
+function setToken(token) {
+  localStorage.setItem('token', token)
+}
+
+onMounted(async () => {
+  await fetchToken()
+  await leagueService.fetchData()
+})
+</script>
+
+<style scoped>
+main {
+  width: 90%;
+  margin: 0 auto;
+  padding: 60px 0 0 0;
+  height: calc(100dvh - 100px);
 }
 </style>
